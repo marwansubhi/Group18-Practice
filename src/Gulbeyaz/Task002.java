d90fbcdced2895d1410ec4b612d4298b1a84c138
alexefdal <alex@agentalex.us>
2020-11-12 13:06:12 -0500

    added comment to Gulbeyaz, Task002

--- src/Gulbeyaz/Task002.java
+++ src/Gulbeyaz/Task002.java
@@ -3,4 +3,6 @@ package Gulbeyaz;
 public class Task002 {
     //I am super excited about this guys!! I can see us writing codes
     //and sharing with each other.. eventually merging. seeing the final result
+
+    //I agree, this is super cool :-))) - alex.
 }
