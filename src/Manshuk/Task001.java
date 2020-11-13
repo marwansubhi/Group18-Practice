78b14ddec1290cfb35c5e4e7f7bdffe766f9b35b
manshuk89 <manshuk.karasaeva@gmail.com>
2020-11-13 07:06:19 -0500

    Added some notes on task1

--- src/Manshuk/Task001.java
+++ src/Manshuk/Task001.java
@@ -1,4 +1,9 @@
 package Manshuk;
 
 public class Task001 {
+    public static void main(String[] args) {
+        System.out.println("Good morning. We have grooming meeting today");
+        //Testing
+        //testing
+    }
 }
