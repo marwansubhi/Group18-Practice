07bf033d847d1284afac43efdf1ebe6b0c327f91
Marwan Subhi <marwan.subhi4@gmail.com>
2020-11-13 07:56:10 -0500

    Added count message

--- src/marwanSubhi/TC004.java
+++ src/marwanSubhi/TC004.java
@@ -5,7 +5,7 @@ public class TC004 {
         System.out.println("Hello world");
 
         for (int i =1; i<=10; i++){
-            System.out.println(i);
+            System.out.println("count "+i);
         }
     }
 }
