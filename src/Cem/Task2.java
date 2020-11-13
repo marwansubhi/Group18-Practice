769888c226c9e430fda8b63ac9c4ff0ed79de61e
cemcapraz <cemcapraz@yahoo.com>
2020-11-13 14:06:51 -0500

    Cem added Task1,2,3

---
+++ src/Cem/Task2.java
@@ -0,0 +1,9 @@
+package Cem;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        // System.out.println("Practice 2");
+    }
+}
---
+++ src/Cem/Task3.java
@@ -0,0 +1,9 @@
+package Cem;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        // System.out.println("Practice 3");
+    }
+}
