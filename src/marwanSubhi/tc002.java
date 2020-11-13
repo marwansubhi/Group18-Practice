8923687edcc130d575d3e8567f70e8d6e9ce3fb1
Marwan Subhi <marwan.subhi4@gmail.com>
2020-11-13 06:48:01 -0500

    Added another note

--- src/marwanSubhi/tc002.java
+++ src/marwanSubhi/tc002.java
@@ -3,5 +3,6 @@ package marwanSubhi;
 public class tc002 {
     public static void main(String[] args) {
         System.out.println("Good Morning");
+        // hope we all have a good day today
     }
 }
