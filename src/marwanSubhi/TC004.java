4d30c5ee30298642b09123488f364aa6dc21bd22
Marwan Subhi <marwan.subhi4@gmail.com>
2020-11-13 07:52:09 -0500

    Added 1 -10 counter

--- src/marwanSubhi/TC004.java
+++ src/marwanSubhi/TC004.java
@@ -3,5 +3,9 @@ package marwanSubhi;
 public class TC004 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        for (int i =1; i<=10; i++){
+            System.out.println(i);
+        }
     }
 }
